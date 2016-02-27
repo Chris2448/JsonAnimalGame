e3f92be239212fa5be35cab0cb00fc523b021cd8
Chris <christopher.m.2448@gmail.com>
2016-02-26 23:44:14 -0500

    Complete

---
+++ Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Client
+{
+    class Program //Client side
+    {
+        public static void Send(StreamWriter writer)
+        {
+            //Send messages to Server
+            var sendtoserverMessage = Console.ReadLine();
+            var messageToSend = JsonConvert.SerializeObject(sendtoserverMessage);
+            writer.WriteLine(messageToSend);
+            writer.Flush();
+        }
+
+        public static void Receive(StreamReader reader)
+        {
+            //Receive messages from Server
+            var receivefromserverMessage = JsonConvert.DeserializeObject<string>(reader.ReadLine());
+            Console.WriteLine("Server:" + receivefromserverMessage);
+        }
+        static void Main(string[] args)
+        {
+            var tcpClient = new TcpClient("127.0.0.1", 1988);
+            Console.WriteLine("----------------------CLIENT--------------------------");
+            var stream = tcpClient.GetStream();
+            Console.WriteLine("Successfully connected to the server");
+            Console.WriteLine("");
+            Console.WriteLine("WELCOME TO THE ANIMAL GUESSING GAME!");
+            Console.WriteLine("");
+            Console.WriteLine("Think of one of the following animals: Eagle, Dog, Fish, Cobra, Crab, or Gorilla");
+            Console.WriteLine("You will be asked a series of questions. Simply answer yes or no.");
+            Console.WriteLine("");
+
+            //Read and Write
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream);
+
+            //Data exchange
+            while (true)
+            {
+                Receive(reader);
+                Send(writer);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace Server
+{
+    class Program //Server side
+    {
+        public static void Send(StreamWriter writer, String sendtoclientMessage)
+        {
+            //Send message to Client
+            var jsonMessage = JsonConvert.SerializeObject(sendtoclientMessage);
+            writer.WriteLine(jsonMessage);
+            writer.Flush();
+        }
+
+        public static string Receive(StreamReader reader,TcpClient tcpClient)
+        { 
+            //Receive message from Client
+            var receivefromclientMessage = JsonConvert.DeserializeObject<string>(reader.ReadLine());
+            Console.WriteLine("Client:" + receivefromclientMessage);
+
+            
+            return receivefromclientMessage;
+        }
+
+        static void Main(string[] args)
+        {
+            //Establish connection
+            var tcpListener = new TcpListener(IPAddress.Any, 1988);
+            tcpListener.Start();
+            Console.WriteLine("------------------------SERVER----------------------------");
+            var tcpClient = tcpListener.AcceptTcpClient();
+            Console.WriteLine("Client has succesfully connected");
+            var stream = tcpClient.GetStream();
+
+            //Read and Write
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream);
+
+            //Questionnaire
+            var questionsList = new Dictionary<string, string>
+            {
+                {"Fly", "Does it fly?"},
+                {"FourLegs", "Does it have four legs?"},
+                {"Swim", "Does it swim?"},
+                {"Shell", "Does it have a shell?"},
+                {"Thumb", "Does it have thumbs?"},
+                {"Poison","Is it poisonous?" }
+
+            };
+
+            //Create List of Animals
+            var animalsList = new List<Animal>
+            {
+                new Animal()
+                {
+                    Name = "Eagle",
+                    Fly = true,
+                    FourLegs = false,
+                    Shell = false,
+                    Swim = false,
+                    Thumb = false,
+                    Poison = false
+                },
+                new Animal()
+                {
+                    Name = "Dog",
+                    Fly = false,
+                    FourLegs = true,
+                    Shell = false,
+                    Swim = false,
+                    Thumb = false,
+                    Poison = false
+                },
+                new Animal()
+                {
+                    Name = "Fish",
+                    Fly = false,
+                    FourLegs = false,
+                    Shell = false,
+                    Swim = true,
+                    Thumb = false,
+                    Poison = false
+                },
+                new Animal()
+                {
+                    Name = "Crab",
+                    Fly = false,
+                    FourLegs = false,
+                    Shell = true,
+                    Swim = true,
+                    Thumb = false,
+                    Poison = false
+                },
+                new Animal()
+                {
+                    Name = "Gorilla",
+                    Fly = false,
+                    FourLegs = false,
+                    Shell = false,
+                    Swim = false,
+                    Thumb = true,
+                    Poison = false
+                },
+                new Animal()
+                {
+                    Name = "Cobra",
+                    Fly = false,
+                    FourLegs = false,
+                    Shell = false,
+                    Swim = false,
+                    Thumb = false,
+                    Poison = true
+                },
+
+            };
+
+            //Animal List Copy
+            var animalListCopy = new List<Animal>();
+            animalListCopy.AddRange(animalsList);
+
+            //Game
+            foreach (var question in questionsList)
+            {
+                Send(writer, question.Value);
+                var answer = Receive(reader, tcpClient) == "yes";
+
+                foreach (var animal in animalsList)
+                {
+                    var property = animal.GetType().GetProperty(question.Key);
+                    var attribute = (bool)property.GetValue(animal);
+
+                    if (attribute != answer)
+                    {
+                        animalListCopy.Remove(animal);
+                    }
+
+                }
+
+                if (animalListCopy.Count == 1)
+                    break;
+            }
+
+            if (animalListCopy.Count == 1)
+                Send(writer, "Your animal is a(n) " + animalListCopy.ElementAt(0).Name);
+            else
+            {
+                Send(writer, "Animal not found");
+            }
+
+        }
+    }
+}
+
